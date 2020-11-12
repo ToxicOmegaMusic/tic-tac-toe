4b780a01eb5387c7a6353cdb216dce9794f905ff
ToxicOmega <pickichu@icloud.com>
2020-11-12 17:08:16 -0500

    Gameplay mechanics finished.

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,36 @@
 let tiles = [];
 let text = document.querySelector("#textArea");
 let turn = undefined;
+let reset = document.querySelector("#reset");
 
+const clickTile = function(x) {
+	let tID = x.id.substring(1);
+	if (tiles[tID-1].textContent == "") {
+		tiles[tID-1].textContent = turn ? "X" : "O";
+		text.textContent = `It is ${(turn = !turn) ? "X" : "O"}'s turn!`;
+	}
+	check();
+}
 
+const check = function() {
 
+	// Check if the game is won and which player would be the winner
+	// Also check for stalemates
 
-
-
+	// If game is won
+	// reset.style.display = "inline";
+}
 
 const init = function() {
+	reset.style.display = "none";
 	tiles = [];
 	for (tile of document.querySelectorAll(".tile")) {
-		tiles.push(tile);
 		tile.textContent = "";
+		tile.setAttribute("onclick", `clickTile(${tile.id})`);
+		tiles.push(tile);
 	}
-
-	text.textContent = `It is ${turn = Math.floor(Math.random() * 2) ? "X" : "O"}'s turn!`;
+	turn = Math.floor(Math.random() * 2)
+	text.textContent = `It is ${turn ? "X" : "O"}'s turn!`;
 }
 
 init();
